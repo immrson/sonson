7d6c2cca21f319a9b6267a3d003c5d70922b9828
Jongmin Kim <whdalsrnt@gmail.com>
2018-08-24 07:30:23 +0000

    update source

--- index.py
+++ index.py
@@ -2,14 +2,14 @@ import yaml
 from flask import Flask, render_template
 app = Flask(__name__)
 
-CONFIG_PATH = '../conf/fc_devops.conf'
+CONFIG_PATH = '../conf/flask.conf'
 
 @app.route("/")
 def hello():
     conf = loadConfig()
     title = conf['global']['title']
-    env = conf['global']['env']
-    return render_template("index.html", title=title, env=env)
+    host = conf['global']['host']
+    return render_template("index.html", title=title, host=host)
     
 def loadConfig():
     with open(CONFIG_PATH, 'r') as f:
